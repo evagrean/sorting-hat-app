ae9ef917d6469af352df9eccf2ed986733439006
eva <me@example.com>
2021-01-07 12:46:53 +0100

    changed sorting button text

--- src/components/SortingButton.js
+++ src/components/SortingButton.js
@@ -4,7 +4,7 @@ const SortingButton = ({ fetchHouseData }) => {
   return (
     <div className="sorting-btn flex flex-col items-center">
       <button className="uppercase px-20 py-9 border-white rounded-md bg-yellow-900 bg-opacity-80 hover:bg-yellow-900 hover:bg-opacity-70 text-white font-bold max-w-max shadow-sm hover:shadow-md text-5xl" onClick={fetchHouseData}>
-        put on the hat
+        put on the sorting hat
       </button>
     </div>
   );
