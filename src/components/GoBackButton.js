f5b2666dfb19072f731e3ed4c9e6cbcad32a9522
eva <me@example.com>
2021-01-14 09:12:30 +0100

    changed backButton styling

--- src/components/GoBackButton.js
+++ src/components/GoBackButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const GoBackButton = ({ goBackToCeremony, textColor, bgColor }) => {
   return (
-    <button className={`back-btn flex items-center justify-center p-3 uppercase text-2xl font-bold bg-opacity-50 hover:bg-opacity-60 rounded-lg shadow-sm ${textColor} ${bgColor}`} onClick={goBackToCeremony}>
+    <button className={`back-btn flex items-center justify-center p-3 uppercase text-2xl font-bold hover:bg-yellow-900 hover:bg-opacity-10 ${textColor}`} onClick={goBackToCeremony}>
       <span className="mr-2 text-4xl font-bold">&#8592;</span>
       <span className="">back to ceremony</span>
     </button>
