eca7f5334d965302165feb19b951e9fc5a963d66
eva <me@example.com>
2021-01-14 09:50:10 +0100

    changed positioning and text color for sortingButton and hatImg

--- src/components/HatImage.js
+++ src/components/HatImage.js
@@ -5,7 +5,7 @@ const HatImage = () => {
   return (
     <div className="hat-img mt-20 sm:mt-30 lg:mt-36">
       <img src={hatImg} alt="sorting hat" className="w-40 h-30 animate-bounce" />
-      <p className=" text-pink-900 font-bold text-4xl text-center animate-pulse">sorting... </p>
+      <p className=" text-white font-semibold text-4xl text-center animate-pulse">sorting... </p>
     </div>
   );
 };
--- src/components/SortingButton.js
+++ src/components/SortingButton.js
@@ -3,7 +3,7 @@ import hatImg from "../images/sorting-hat.png";
 
 const SortingButton = ({ fetchHouseData }) => {
   return (
-    <button className="flex flex-col items-center lg:flex-row lg:items-center sorting-btn mt-20 sm:my-60 mx-10 p-8 text-white  font-bold bg-yellow-900 bg-opacity-80 hover:bg-yellow-900 hover:bg-opacity-70 rounded-lg shadow-2xl hover:shadow-md z-10 " onClick={fetchHouseData}>
+    <button className="flex flex-col items-center lg:flex-row lg:items-center sorting-btn mt-20 sm:mt-52 mx-10 p-8 text-white  font-bold bg-yellow-900 bg-opacity-80 hover:bg-yellow-900 hover:bg-opacity-70 rounded-lg shadow-2xl hover:shadow-md z-10 " onClick={fetchHouseData}>
       <span className="mb-3">
         <img src={hatImg} alt="hat" className="w-20 h-25 z-0" />
       </span>
